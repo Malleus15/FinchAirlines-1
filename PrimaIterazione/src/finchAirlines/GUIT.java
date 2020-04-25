6270275e25cf6cc5b19840908052f38ca8f8916b
matte <matte@DESKTOP-82B7S8D>
2020-04-25 11:08:58 +0200

    Added some flights and fixed duplicated listaVoli

--- PrimaIterazione/src/finchAirlines/GUIT.java
+++ PrimaIterazione/src/finchAirlines/GUIT.java
@@ -14,7 +14,7 @@ public class GUIT {
 		finchAirlines.setListaClienti(listaClienti);
 		
 		/*Riempimento listaVoli di FinchAirlines*/
-		ArrayList<Volo> listaVoli = new ArrayList<Volo>();
+		ArrayList<Volo> listaVoliDisponibili = new ArrayList<Volo>();
 		Aeroporto aeroporto1 = new Aeroporto("Aeroporto Internazionale di Catania Vincenzo Bellini", "Catania", "CTA");
 		Aeroporto aeroporto2 = new Aeroporto("Aeroporto Intercontinentale di Roma Leonardo da Vinci", "Roma", "FCO");
 		Aeroporto aeroporto3 = new Aeroporto("Aeroporto Intercontinetale di Milano Malpensa", "Milano", "MXP");
@@ -23,11 +23,49 @@ public class GUIT {
 		
 		aeroporti[0] = aeroporto1;
 		aeroporti[1] = aeroporto2;
-		Volo volo1 = new Volo(new Date(2020, 2, 15, 17, 00), new Date(2020, 2, 15, 18, 15), new DescrizioneVolo("FA1234", 30, aeroporti));
+		Volo volo1 = new Volo(new Date(2020, 2, 15, 17, 00), new Date(2020, 2, 15, 18, 15), new DescrizioneVolo("FA0001", 30, aeroporti));
+		
+		
+		aeroporti[0] = aeroporto1;
+		aeroporti[1] = aeroporto2;
+		Volo volo2 = new Volo(new Date(2020, 2, 15, 20, 00), new Date(2020, 2, 15, 21, 15), new DescrizioneVolo("FA0002", 15, aeroporti));
+		
+		aeroporti[0] = aeroporto1;
+		aeroporti[1] = aeroporto2;
+		Volo volo3 = new Volo(new Date(2020, 2, 15, 19, 00), new Date(2020, 2, 15, 20, 15), new DescrizioneVolo("FA0003", 45, aeroporti));
 		
 		aeroporti[0] = aeroporto2;
 		aeroporti[1] = aeroporto3;
-		Volo volo2 = new Volo(new Date(2020, 2, 22, 20, 00), new Date(2020, 2, 22, 20, 50), new DescrizioneVolo("FA4321", 15, aeroporti));
+		Volo volo4 = new Volo(new Date(2020, 2, 22, 20, 00), new Date(2020, 2, 22, 20, 50), new DescrizioneVolo("FA0011", 15, aeroporti));
+		
+		aeroporti[0] = aeroporto2;
+		aeroporti[1] = aeroporto3;
+		Volo volo5 = new Volo(new Date(2020, 2, 22, 13, 30), new Date(2020, 2, 22, 14, 20), new DescrizioneVolo("FA0012", 50, aeroporti));
+		
+		aeroporti[0] = aeroporto2;
+		aeroporti[1] = aeroporto3;
+		Volo volo6 = new Volo(new Date(2020, 2, 22, 20, 00), new Date(2020, 2, 22, 20, 50), new DescrizioneVolo("FA0013", 15, aeroporti));
+		
+		aeroporti[0] = aeroporto2;
+		aeroporti[1] = aeroporto3;
+		Volo volo7 = new Volo(new Date(2020, 2, 15, 13, 30), new Date(2020, 2, 15, 14, 20), new DescrizioneVolo("FA0014", 40, aeroporti));
+		
+		aeroporti[0] = aeroporto2;
+		aeroporti[1] = aeroporto4;
+		Volo volo8 = new Volo(new Date(2020, 2, 15, 17, 00), new Date(2020, 2, 15, 18, 15), new DescrizioneVolo("FA1001", 30, aeroporti));
+		
+		aeroporti[0] = aeroporto2;
+		aeroporti[1] = aeroporto4;
+		Volo volo9 = new Volo(new Date(2020, 2, 22, 20, 00), new Date(2020, 2, 22, 20, 50), new DescrizioneVolo("FA1002", 15, aeroporti));
+		
+		aeroporti[0] = aeroporto3;
+		aeroporti[1] = aeroporto1;
+		Volo volo10 = new Volo(new Date(2020, 2, 29, 21, 00), new Date(2020, 2, 29, 23, 15), new DescrizioneVolo("FA0101", 30, aeroporti));
+		
+		aeroporti[0] = aeroporto3;
+		aeroporti[1] = aeroporto1;
+		Volo volo11 = new Volo(new Date(2020, 2, 29, 19, 00), new Date(2020, 2, 29, 21, 15), new DescrizioneVolo("FA0102", 35, aeroporti));
+		
 		
 		
 		
